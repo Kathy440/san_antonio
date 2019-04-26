1691089468af7c9fa98376dfd0c805bcc3151472
Kathy <mkathy.voisin@gmail.com>
2019-04-26 15:27:41 +0200

    Suite de l'exo et création new branche pour faire des tests

--- san_antonio.py
+++ san_antonio.py
@@ -4,12 +4,22 @@ quotes = [
     "On doit pouvoir choisir entre s'écouter parler et se faire entendre."
 ]
 
-characters = [
-    "alvin et les Chipmunks", 
-    "Babar", 
-    "betty boop", 
-    "calimero", 
-    "casper", 
-    "le chat potté", 
-    "Kirikou"
-]
+characters = ["alvin et les Chipmunks", "Babar", "betty boop", "calimero", "casper", "le chat potté", "Kirikou"]
+
+def show_random_quote(my_list):
+      # get a random number
+  item = my_list[0]
+  return item
+
+user_answer = input("Tapez entrée pour connaître une autre citation ou B pour quitter le programme.")
+
+# Tant que la réponse de l'utilisateur n'ai pas B
+while user_answer != "B":
+    print(show_random_quote(quotes)) #Affiche une citation
+    user_answer = input("Tapez entrée pour connaître une autre citation ou B pour quitter le programme.")
+
+for character in characters:
+  name_character = character.capitalize()
+  print(name_character)
+    
+print(show_random_quote(quotes))
